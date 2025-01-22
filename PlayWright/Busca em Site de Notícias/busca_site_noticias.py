30624fc4ea7470b470d42c455b183c3f4335da9f
Augusto Lima <losgutbov@gmail.com>
2025-01-22 17:56:43 -0300

    First solution with playwright

--- PlayWright/Busca em Site de Notícias/busca_site_noticias.py
+++ PlayWright/Busca em Site de Notícias/busca_site_noticias.py
@@ -1,17 +1,13 @@
 from playwright.sync_api import sync_playwright
 
 
-# with sync_playwright() as p:
-#     browser = p.chromium.launch(headless=False)
-#     page = browser.new_page()
-#     page.goto("http://playwright.dev")
-#     print(page.title())
-#     browser.close()
-
-
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
-    page.goto(" https://www.bbc.com/news")
-    print(page.title())
+    page.goto("https://www.bbc.com/news")
+    page.get_by_label("Search BBC").click()
+    page.get_by_placeholder("Search news, topics and more").fill("technology")
+    page.get_by_test_id("search-input-search-button").click()
+    for titulo in page.get_by_test_id("card-headline").all()[0:5]:
+        print(titulo.text_content())
     browser.close()
